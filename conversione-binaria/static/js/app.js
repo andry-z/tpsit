cb07ccc0968681b2b37a4583fae90d6ab6b6bf34
andry-z <andryzotti051@gmail.com>
2023-02-23 09:50:24 +0100

    Script modificato

--- conversione-binaria/static/js/app.js
+++ conversione-binaria/static/js/app.js
@@ -1,16 +1,18 @@
-function convertAndDisplay() {
+let binary = "";
+
+let submitButton = document.getElementById("button");
+submitButton.addEventListener("click", function () {
+  binary = "";
   let inputNumber = document.getElementById("inputNumber").value;
-  let binary = "";
-
-  function convert(n) {
-    if (n == 0) {
-      return binary;
-    } else {
-      binary = (n % 2) + binary;
-      return convert((n - (n % 2)) / 2);
-    }
-  }
-
   let binaryResult = document.getElementById("binaryResult");
   binaryResult.innerHTML = "Il numero binario è: " + convert(inputNumber);
+});
+
+function convert(n) {
+  if (n == 0) {
+    return binary;
+  } else {
+    binary = (n % 2) + binary;
+    return convert((n - (n % 2)) / 2);
+  }
 }
